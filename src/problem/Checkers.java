9e739f943e9f63cd6a61059e68d62442a062a78f
Brandon Dean <brandondean1068@gmail.com>
2025-04-21 09:51:41 -0400

    Added Diagonal and jump moves to remaining moves and jump moves and distances from the back row

--- src/problem/Checkers.java
+++ src/problem/Checkers.java
@@ -43,36 +43,72 @@ public class Checkers implements Game<Square> {
     }
 
     public int utility(){
+        //Could try incorporating space jumps,
+        //and distance from other side
+
         //This is where we do our checks
         int dSum = 0; //Check diagonals, this shows how many diagonal conflicts
         for (int i = 0; i < BOARD_SIZE; i++){
+            Integer hop = 0;
             Square square = new Square(i, i);
             //Make new square to check with
             if (board.containsKey(square)){
                 //board should contain the new square unless it's out of bounds
-                if (board.get(square) == Mark.R) {
+                if (board.get(square) == Mark.R) { //Distance from other side
+                    dSum = BOARD_SIZE - i + (dSum + 1);
+                }else {
+                    dSum = BOARD_SIZE - i + (dSum - 1);
+                }
+                if (board.get(new Square(i-1, i-1)) == Mark.B) {
                     dSum++;
-                }else{
-                    dSum--;
+                }
+                if (board.get(new Square(i+1, i+1)) == Mark.B){
+                    dSum++;
                 }
             }
         }
-        if (dSum == BOARD_SIZE){ //Max won
+        if (dSum >= BOARD_SIZE){ //Max won
             return 1;
-        }else if (dSum == -BOARD_SIZE){ //Min won
+        }else if (dSum <= -BOARD_SIZE){ //Min won
             return -1;
         }
         return 0; //No one won
     }
 
-    public List<Square> getAllRemainingMoves(){
+    public List<Square> getAllRemainingMoves() {
         //Only remaining moves can be in diagonals
+        //Going to have to include bit where it limits the amount of rows
         List<Square> result = new ArrayList<>();
-        for (int row = 0; row < BOARD_SIZE; row++){
+        for (int row = 0; row < BOARD_SIZE; row++) {
             Square square = new Square(row, row);
-            if (!board.containsKey(square)){
-                    result.add(square);
+            Square uright_di = new Square(row + 1, row + 1);
+            Square dleft_di = new Square(row - 1, row - 1);
+            Square dright_di = new Square(row + 1, row - 1);
+            Square uleft_di = new Square(row - 1, row + 1);
+            if (!board.containsKey(square)) {
+                result.add(square);
             }
+            //Adds diagonals to possible moves
+            else if (!board.containsKey(uright_di)) {
+                result.add(uright_di);
+            } else if (!board.containsKey(dleft_di)) {
+                result.add(dleft_di);
+            } else if (!board.containsKey(dright_di)) {
+                result.add(dright_di);
+            } else if (!board.containsKey(uleft_di)) {
+                result.add(uleft_di);
+            }
+            //Adds jumps to possible moves
+            else if (board.get(uright_di) == Mark.B) {
+                if (!board.containsKey(new Square(row + 2, row + 2))) result.add(new Square(row + 2, row + 2));
+            } else if (board.get(dleft_di) == Mark.B) {
+                if (!board.containsKey(new Square(row - 2, row - 2))) result.add(new Square(row - 2, row - 2));
+            } else if (board.get(dright_di) == Mark.B) {
+                if (!board.containsKey(new Square(row + 2, row - 2))) result.add(new Square(row + 2, row - 2));
+            } else if (board.get(uleft_di) == Mark.B) {
+                if (!board.containsKey(new Square(row - 2, row + 2))) result.add(new Square(row - 2, row + 2));
+            }
+
         }
         return result;
     }
